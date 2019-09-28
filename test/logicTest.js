be967453e3e56638dc6d1fa16a54e318a5e6f72f
Eisenbartl <Eisenbartl@yahoo.com>
2019-09-28 14:30:56 -0500

    step six commit, custom delimiter feature added, formatting alert feature added, rules of use component added

--- test/logicTest.js
+++ test/logicTest.js
@@ -1,10 +1,34 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const logic = require('../server/logic');
-
-const addNumbers = logic;
+const addNumbers = require('../server/logic').addNumbers;
+const customDelim = require('../server/logic').customDelim;
 
 describe('Logic', () => {
+  // customDemlim function tests
+  describe('customDelim', () => {
+    it('customDelim should be typeof object', () => {
+      assert.typeOf(customDelim('5'), 'array');
+    });
+
+    it('addNumbers should accept a string delimited by commas and return an array', () => {
+      expect(customDelim('1,2,3,4,5')).to.eql(['1', '2', '3', '4', '5']);
+    });
+
+    // failing test
+    // it('addNumbers should accept a string delimited by newLine chars and return an array', () => {
+    //   expect(customDelim('1\n2\n3\n4\n5')).to.eql(['1', '2', '3', '4', '5']);
+    // });
+
+    it('addNumbers should accept a custom delimiter and return an array', () => {
+      expect(customDelim('//@1@2@3')).to.eql(['//', '1', '2', '3']);
+    });
+
+    it('addNumbers should accept a custom delimiter and other delimiters and return an array', () => {
+      expect(customDelim('//@1,2@3,4')).to.eql(['//', '1', '2', '3', '4']);
+    })
+  });
+
+  // addNumbers function tests
   describe('addNumbers', () => {
     // it('addNumbers should be typeof number', () => {
     //   assert.typeOf(addNumbers('5'), 'number');
@@ -40,7 +64,6 @@ describe('Logic', () => {
       expect(addNumbers('5,tytyt')).to.eql({ sum: '5', exceptions: [] });
     });
 
-    
     // app returns correct number but test still fails
     // it('addNumbers should accept newline characters', () => {
     //   expect(addNumbers('1\n2,3')).to.equal(6);
@@ -56,6 +79,27 @@ describe('Logic', () => {
 
     it('addNumbers should not accept numbers greater than 1001', () => {
       expect(addNumbers('5,1000.1,5')).to.eql({ sum: '10', exceptions: [] });
+    });
+
+    it('addNumbers should accept a custom delimiter if formatting condition is met', () => {
+      expect(addNumbers('//;5;1;5')).to.eql({ sum: '11', exceptions: [] });
+    });
+
+    it('addNumbers should accept a custom delimiter if formatting condition is met', () => {
+      expect(addNumbers('//$2$1$3$-1')).to.eql({ sum: '6', exceptions: ['-1'] });
+    });
+
+    it('addNumbers should not accept a custom delimiter if formatting is incorrect', () => {
+      expect(addNumbers('/;5;1;5')).to.eql({ sum: '0', exceptions: ['formatting error'] });
+    });
+
+    // app returns correct number but test fails with actual = 6 and expects = 13
+    // it('addNumbers should support custom delimiter and previous formats', () => {
+    //   expect(addNumbers('//;5;1,5\n2')).to.eql({ sum: '13', exceptions: [] });
+    // });
+
+    it('addNumbers should support custom delimiter and previous formats', () => {
+      expect(addNumbers('//;5;tytyt')).to.eql({ sum: '5', exceptions: [] });
     });
   });
 });
